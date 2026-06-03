01c0d20a1126170b0069691c93259ef1d7aec029
Yashwanth Reddy Puli <Yashureddy4044@gmail.com>
2026-06-04 01:38:42 +0530

    Update bot.js
    Updated Presence

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Playing /help | Ragnir Bot",
+        name: "/help | Ragnir Bot",
         // Activity type number (0 = Playing).
         type: 0, 
       },
